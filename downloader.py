8c132b37690a250268561fa16fedf3de87a68cd3
Sanket Patel <sanketplus@gmail.com>
2016-09-02 15:37:02 +0530

    added downaloader

---
+++ downloader.py
@@ -0,0 +1,15 @@
+import wget
+import os
+
+
+def wget_get(url):
+    out_path = os.path.expanduser("~")+"/Music/always320/"
+
+    if not os.path.exists(out_path):
+        os.makedirs(out_path)
+
+    file_name = out_path + url.split("/")[-1]
+
+    downloaded = wget.download(url,file_name)
+
+    print downloaded
