8792ae41ad27322fdca4b1dd239f0ed5d7886d30
Parth Parikh <parthpower@gmail.com>
2017-11-08 23:56:50 -0800

    setup.py update

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
 from setuptools import setup
 
-
 setup(
-    name="always320",
-    author=["Parth Parikh","Sanket Patel"],
-    author_email=["parthpower@gmail.com","sanketplus@gmail.com"],
-    maintainer=["Parth Parikh","Sanket Patel"],
-    maintainer_email=["parthpower@gmail.com","sanketplus@gmail.com"],
-    version="0.2.11",
-    packages=['always320'],
+	name="always320",
+	author=["Parth Parikh","Sanket Patel"],
+	author_email=["parthpower@gmail.com","sanketplus@gmail.com"],
+	maintainer=["Parth Parikh","Sanket Patel"],
+	maintainer_email=["parthpower@gmail.com","sanketplus@gmail.com"],
+	version="0.2.11",
+	packages=['always320'],
     entry_points={
         'console_scripts': [
             'get320=always320.get320:main',
         ],
     },
-    long_description='''# always320
+	long_description='''# always320
 Download [amlost] always high quality mp3 from YouTube.
 
 `Usage: get320.py [-f file_name][Song Name]`
 
 Specify a song name or input file with song name in each line.
-    
-    ''',
-    install_requires=['beautifulsoup4','html5lib','robobrowser','mechanize'],
+	
+	''',
+	install_requires=['beautifulsoup4','html5lib','robobrowser','mechanize'],
 )
