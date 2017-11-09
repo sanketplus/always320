a691b2454a8c6d6d757eefe9eeb3f9259d40859f
Parth Parikh <parthpower@users.noreply.github.com>
2017-11-08 23:09:17 -0800

    Fixed Beautifulsoup typo

--- always320/vubey_api.py
+++ always320/vubey_api.py
@@ -1,26 +1,26 @@
 try:
-    import BeautifulSoup
+    # Python 2
+    from BeautifulSoup import BeautifulSoup
 except Exception as e:
+    # Python 3
     from bs4 import BeautifulSoup
-
-
-
 
 def get_mp3_url(yt_url):
     try:
         import mechanize
-        b = mechanize.Browser()
-        b.open("https://vubey.yt")
-        b.select_form('wf-form-signup-form')
-        b.form['videoURL'] = yt_url
-        fs = b.submit()
-        d = fs.get_data()
-
-        bs = BeautifulSoup(d)
-        links = bs.findAll("a")
-
-        link = [l for l in links if "here" in l]
-        url = link[0].get("href")
-        return (url,url.split('/')[-1])
     except Exception as e:
         raise e
+    b = mechanize.Browser()
+    b.open("https://vubey.yt")
+    b.select_form('wf-form-signup-form')
+    b.form['videoURL'] = yt_url
+    fs = b.submit()
+    d = fs.get_data()
+
+    bs = BeautifulSoup(d)
+    links = bs.findAll("a")
+
+    link = [l for l in links if "here" in l]
+    url = link[0].get("href")
+    return (url,url.split('/')[-1])
+
